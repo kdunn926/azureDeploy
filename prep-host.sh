4cf09a7b02a535d3fd92f314146f8ae439bdf6e7
Kyle Dunn <kdunn926@gmail.com>
2016-02-26 12:55:00 -0700

    Fix append

--- prep-host.sh
+++ prep-host.sh
@@ -3,6 +3,6 @@
 IPADDR=$1
 HOSTNAME=$2
 
-echo -e "\n${IPADDR} ${HOSTNAME}" >> /etc/hosts
+echo -e "\n${IPADDR} ${HOSTNAME}" | sudo tee --append /etc/hosts
 ssh-keyscan ${HOSTNAME} >> ~/.ssh/known_hosts
 
