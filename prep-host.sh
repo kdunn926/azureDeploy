56bdb5d65b7376f9f1b3854b7b541fee36a4e9ca
Kyle Dunn <kdunn926@gmail.com>
2016-02-26 18:41:52 -0700

    Bitbucket script locations no good, switching to github

--- prep-host.sh
+++ prep-host.sh
@@ -2,7 +2,8 @@
 
 IPADDR=$1
 HOSTNAME=$2
+USER=$3
 
-echo -e "\n${IPADDR} ${HOSTNAME}" | sudo tee --append /etc/hosts
-ssh-keyscan ${HOSTNAME} >> ~/.ssh/known_hosts
+echo -e "\n${IPADDR} ${HOSTNAME}" | tee --append /etc/hosts
+ssh-keyscan ${HOSTNAME} | tee --append ~${USER}/.ssh/known_hosts
 
