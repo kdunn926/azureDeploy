96120e3ba64b8d7bb1ab64d015f1e3bdb87eb25c
Kyle Dunn <kdunn926@gmail.com>
2016-02-26 21:37:52 -0700

    removing unused parameter

--- prep-host.sh
+++ prep-host.sh
@@ -2,7 +2,6 @@
 
 IPADDR=$1
 HOSTNAME=$2
-USER=$3
 
 # Enable IP<->hostname mapping
 echo -e "\n${IPADDR} ${HOSTNAME}" | tee --append /etc/hosts
@@ -14,10 +13,5 @@ sed -i -e 's/ALL$/NOPASSWD: ALL/' /etc/sudoers.d/waagent
 sed -i -e 's/^Defaults    requiretty/#Defaults    requiretty/' /etc/sudoers
 
 # Allow loopback SSH
-mkdir -p ~${USER}/.ssh 
-chown -R ${USER}:${USER} ~${USER}
-chmod 755 /home/${USER}
-
-ssh-keyscan ${HOSTNAME} | tee --append ~${USER}/.ssh/known_hosts
-chmod 600 ~${USER}/.ssh
+ssh-keyscan ${HOSTNAME} | tee --append /etc/ssh/ssh_known_hosts
 
