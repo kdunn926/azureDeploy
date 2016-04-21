f24ff0d00898cda85d4f6b6543b4807b97345f5b
Kyle Dunn <kdunn926@gmail.com>
2016-04-20 21:31:04 -0600

    Adding some bits

--- prep-host.sh
+++ prep-host.sh
@@ -15,3 +15,35 @@ sed -i -e 's/^Defaults    requiretty/#Defaults    requiretty/' /etc/sudoers
 # Allow loopback SSH
 ssh-keyscan ${HOSTNAME} | tee --append /etc/ssh/ssh_known_hosts
 
+# Generate ssh keys
+ssh-keygen -P "" -f /root/.ssh/id_rsa
+for u in `ls /home/ | cut -f2 -d\/` ; do
+    su ${u} -c "ssh-keygen -P '' -f /home/${u}/.ssh/id_rsa"
+done;
+
+# Disable selinux
+sed -ie "s|SELINUX=enforcing|SELINUX=disabled|" /etc/selinux/config
+setenforce 0
+
+# Install an configure fail2ban for the script kiddies
+yum install epel-release -y
+yum install fail2ban -y
+
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+sed -ie "s|ignoreip = 127\.0\.0\.1/8|ignoreip = 127.0.0.1/8\nignoreip = ${IPADDR}/24|" /etc/fail2ban/jail.local
+
+echo -e "\n\n
+[ssh-iptables]
+enabled  = true
+filter   = sshd
+action   = iptables[name=SSH, port=ssh, protocol=tcp]
+logpath  = /var/log/secure
+maxretry = 5
+" >> /etc/fail2ban/jail.local 
+
+service fail2ban start
+chkconfig fail2ban on
+
+yum install xfsprogs -y
+
